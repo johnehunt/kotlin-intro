e503cc49936fb1e0a23720db17732ee8934f73a4
John Hunt <john.hunt10@gmail.com>
2020-07-08 15:09:54 +0100

    Added some additional comments

--- 01-helloworld/src/com/jjh/hello/main/HelloWorldApp.kt
+++ 01-helloworld/src/com/jjh/hello/main/HelloWorldApp.kt
@@ -2,6 +2,12 @@ package com.jjh.hello.main
 
 object HelloWorldApp {
 
+    /**
+     * Defines entry point for a Kotlin application.
+     * Need to indicates that the compiler shoudl generate
+     * byte codes that are compatible with the Java definition
+     * of a static method that it expects to find to run a program
+     */
     @JvmStatic
     fun main(args: Array<String>) {
         println("Hello World!")
