484c00cbfc090ef683173c98e087e0d90605f5cd
John Hunt <john.hunt10@gmail.com>
2020-07-08 16:54:10 +0100

    Split out infix form operator definitions

---
+++ 06-further-classes/src/com/jjh/ops/Counter.kt
@@ -0,0 +1,32 @@
+package com.jjh.ops
+
+data class Counter(var value: Int) {
+
+    operator fun plus(increment: Int): Counter = Counter(value + increment)
+    operator fun plus(other: Counter): Counter = Counter(value + other.value)
+    operator fun minus(decrement: Int): Counter = Counter(value - decrement)
+    operator fun minus(other: Counter): Counter = Counter(value - other.value)
+
+    operator fun plusAssign(c: Counter) {
+        value += c.value
+    }
+}
+
+object CounterApp {
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val c1 = Counter(1)
+        println(c1 + 2)
+        println(c1 - 2)
+
+        val c2 = Counter(5)
+
+        println(c1 + c2)
+        println(c1 - c2)
+
+        c2 += c1
+        println(c2)
+
+    }
+}
--- 06-further-classes/src/com/jjh/ops/QuantityApp.kt
+++ 06-further-classes/src/com/jjh/ops/QuantityApp.kt
@@ -1,18 +1,8 @@
 package com.jjh.ops
 
 data class Quantity(val value: Int) {
-    infix fun add(i: Int): Quantity {
-        return Quantity(value + i)
-    }
-}
-
-data class Counter(var index: Int) {
-    operator fun plus(increment: Int): Counter {
-        return Counter(index + increment)
-    }
-    operator fun plusAssign(c: Counter) {
-        index = index + c.index
-    }
+    infix fun add(i: Int): Quantity = Quantity(value + i)
+    infix fun sub(i: Int): Quantity = Quantity(value -+ i)
 }
 
 object QuantityApp {
@@ -20,16 +10,6 @@ object QuantityApp {
     fun main(args: Array<String>) {
         val q1 = Quantity(5)
         println(q1 add 5)
-
-        println("------")
-
-        val c1 = Counter(1)
-        println(c1 + 2)
-
-        val c2 = Counter(5)
-        c2 += c1
-        println(c2)
-
-
+        println(q1 sub 5)
     }
 }
