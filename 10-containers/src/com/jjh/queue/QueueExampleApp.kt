f5098926a9bd3cb5cbc9082e9a57536aa78c7c69
John Hunt <john.hunt10@gmail.com>
2020-07-09 09:21:25 +0100

    Tidying

--- 10-containers/src/com/jjh/queue/QueueExampleApp.kt
+++ 10-containers/src/com/jjh/queue/QueueExampleApp.kt
@@ -2,23 +2,20 @@ package com.jjh.queue
 
 import kotlin.collections.*
 
-object QueueExampleApp {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val nums: ArrayDeque<Int> = ArrayDeque<Int>(listOf(1, 2, 3))
-        println(nums)
-        val names: ArrayDeque<String> = ArrayDeque<String>(5)
-        names.add("John")
-        names.add("Denise")
-        names.add("Adam")
+fun main(args: Array<String>) {
+    val numbers: ArrayDeque<Int> = ArrayDeque<Int>(listOf(1, 2, 3))
+    println(numbers)
 
-        println(names)
-        println(names.size)
-        println(names.first())
-        println(names.last())
+    val names: ArrayDeque<String> = ArrayDeque<String>(5)
+    names.add("John")
+    names.add("Denise")
+    names.add("Adam")
 
-        println(names.removeFirst())
-        println(names)
+    println(names)
+    println(names.size)
+    println(names.first())
+    println(names.last())
 
-    }
+    println(names.removeFirst())
+    println(names)
 }
