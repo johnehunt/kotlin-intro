4028e151b49b64ec4a5d22f7d41343e95c79e7df
John Hunt <john.hunt10@gmail.com>
2020-07-09 09:11:29 +0100

    Simplified examples

--- 03-memberfunctions-and-properties/src/com/jjh/utils/MathUtilsApp.kt
+++ 03-memberfunctions-and-properties/src/com/jjh/utils/MathUtilsApp.kt
@@ -1,17 +1,10 @@
 package com.jjh.utils
 
-import com.jjh.utils.MathUtils
+fun main(args: Array<String>) {
+    println(MathUtils.ZERO)
+    println(MathUtils.MIN)
+    println(MathUtils.MAX)
 
-object MathUtilsApp {
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println(MathUtils.ZERO)
-        println(MathUtils.MIN)
-        println(MathUtils.MAX)
-
-        MathUtils.add(2, 3)
-        MathUtils.sub(6, 2)
-    }
-
+    MathUtils.add(2, 3)
+    MathUtils.sub(6, 2)
 }
