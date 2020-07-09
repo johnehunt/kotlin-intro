bc17d3919a65839c4f76b26779a0619b022d1245
John Hunt <john.hunt10@gmail.com>
2020-07-09 09:18:12 +0100

    CHanged layout

--- 08-packages/src/com/jjh/main/Main.kt
+++ 08-packages/src/com/jjh/main/Main.kt
@@ -2,10 +2,7 @@ package com.jjh.main
 
 import com.jjh.util.Person
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val p = Person("John", 55)
-        println(p)
-    }
+fun main(args: Array<String>) {
+    val p = Person("John", 55)
+    println(p)
 }
