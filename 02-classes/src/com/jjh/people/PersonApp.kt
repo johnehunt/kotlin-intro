3172a47ee44a3d6fa978a52ebe0475725c7c3ab9
John Hunt <john.hunt10@gmail.com>
2020-07-09 09:08:46 +0100

    Simplified examples

--- 02-classes/src/com/jjh/people/PersonApp.kt
+++ 02-classes/src/com/jjh/people/PersonApp.kt
@@ -1,22 +1,19 @@
 package com.jjh.people
 
-object PersonApp {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val p = Person("John", 54)
-        println(p)
-        println(p.name + " is " + p.age)
-        p.age = 55
-        // p.name = "Bob" // Compilation error as name is a read only val
-        println(p.name + " is " + p.age)
-        println(p)
-        val p1 = Person("John", 55)
-        val p2 = Person("Denise", 52)
-        val p3 = Person("Phoebe", 22)
-        val p4 = Person("Adam", 20)
-        println(p1)
-        println(p2)
-        println(p3)
-        println(p4)
-    }
+fun main(args: Array<String>) {
+    val p = Person("John", 54)
+    println(p)
+    println(p.name + " is " + p.age)
+    p.age = 55
+    // p.name = "Bob" // Compilation error as name is a read only val
+    println(p.name + " is " + p.age)
+    println(p)
+    val p1 = Person("John", 55)
+    val p2 = Person("Denise", 52)
+    val p3 = Person("Phoebe", 22)
+    val p4 = Person("Adam", 20)
+    println(p1)
+    println(p2)
+    println(p3)
+    println(p4)
 }
--- 02-classes/src/com/jjh/people/PersonApp4.kt
+++ 02-classes/src/com/jjh/people/PersonApp4.kt
@@ -1,18 +1,13 @@
 package com.jjh.people
 
-object PersonApp4 {
+fun main(args: Array<String>) {
+    val p1 = Person4("John")
+    println("${p1.name} has partner ${p1.partner}")
+    val p2 = Person4("Denise")
+    p1.partner = p2
+    println("${p1.name} has partner ${p1.partner}")
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val p1 = Person4("John")
-        println("${p1.name} has partner ${p1.partner}")
-        val p2 = Person4("Denise")
-        p1.partner = p2
-        println("${p1.name} has partner ${p1.partner}")
-
-        // Can use secondary constructor for this
-        val p3 = Person4("Phoebe", 23, Person4("Gryff"))
-        println("${p3.name} has partner ${p3.partner}")
-    }
-
+    // Can use secondary constructor for this
+    val p3 = Person4("Phoebe", 23, Person4("Gryff"))
+    println("${p3.name} has partner ${p3.partner}")
 }
