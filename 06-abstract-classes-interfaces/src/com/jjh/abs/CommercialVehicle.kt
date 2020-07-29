05b46c3b9e00e5efc840ac07a4669f718906d542
John Hunt <john.hunt10@gmail.com>
2020-07-29 17:18:51 +0100

    Removed unnecessary inheritance

--- 06-abstract-classes-interfaces/src/com/jjh/abs/CommercialVehicle.kt
+++ 06-abstract-classes-interfaces/src/com/jjh/abs/CommercialVehicle.kt
@@ -1,5 +1,14 @@
 package com.jjh.abs
 
-abstract class CommercialVehicle : Conveyance() {
+abstract class CommercialVehicle {
+    // Abstract property
+    abstract val owner: String
+    // abstract member function
     abstract fun load(contents: String)
+}
+
+class Lorry(override val owner: String) : CommercialVehicle() {
+    override fun load(contents: String) {
+        println("loading $contents")
+    }
 }
