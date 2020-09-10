5c6e24cc660b00d5aeeac853a7f19af365d19183
John Hunt <john.hunt10@gmail.com>
2020-09-10 14:55:49 +0100

    Minor tidying

--- 03-objects-and-control/src/com/jjh/control/TestBreak.kt
+++ 03-objects-and-control/src/com/jjh/control/TestBreak.kt
@@ -1,14 +1,26 @@
 package com.jjh.control
 
-fun main(args: Array<String>) {
+fun main() {
     val y = 10
     val x = 5
+
+    for (i in 0 until y) {
+        if (i == x) {
+            break;
+        }
+        print("i: $i, ");
+    }
+    println()
+
+    println("-------------")
+
     loop@ for (i in 0 until y) {
         for (j in 0 until x) {
             if (i == x) {
                 break@loop
             }
-            println(i.toString() + "\t" + j)
+            println("$i \t $j")
         }
     }
+
 }
--- 03-objects-and-control/src/com/jjh/control/TestIf.kt
+++ 03-objects-and-control/src/com/jjh/control/TestIf.kt
@@ -1,9 +1,21 @@
 package com.jjh.control
 
+import java.util.*
+
 fun main() {
+
+    print("Enter an integer: ")
+    val input = Scanner(System.`in`)
+    val num = input.nextInt()
+    if (num > 0) {
+        println("$num is positive")
+        println("$num squared is ${num * num}")
+    }
+
     // Can have any number of if else and optional else
     val i = 5
     val j = 10
+
     if (i < j) {
         println(i)
     } else if (i == j) {
