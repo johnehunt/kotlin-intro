8443b8acf860d355597ab636f4e73301a77e3153
John Hunt <john.hunt10@gmail.com>
2020-12-11 15:01:37 +0000

    Example of some simple numerical operations and their effects on vars and vals

---
+++ 03-objects-and-control/src/com/jjh/numerics/Main.kt
@@ -0,0 +1,16 @@
+package com.jjh.numerics
+
+fun main() {
+    val a = 12
+    val b = 21
+    //  val c: Byte= a.toByte() + b.toByte();
+    //  print(c)
+
+    // println(a++)   // compilation error
+
+    var count = 0
+    println(++count)
+    println(count++)
+    count += 2
+    println(count)
+}
