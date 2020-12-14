a5117d2d3d066c72bf42e34a4922f9fad9556ebb
John Hunt <john.hunt10@gmail.com>
2020-12-14 16:39:40 +0000

    Added comment indicate dependency on Kotlin 1.4

--- 03-objects-and-control/src/com/jjh/control/TestWhenWithBreakContinue.kt
+++ 03-objects-and-control/src/com/jjh/control/TestWhenWithBreakContinue.kt
@@ -1,11 +1,12 @@
 package com.jjh.control
 
 fun main() {
-//    for (i in 1..10) {
-//        when (i) {
-//            2 -> continue
-//            4 -> break
-//            else -> println(i)
-//        }
-//    }
+    // Requires Kotlin 1.4 to work
+    for (i in 0..10) {
+        when (i) {
+            2 -> continue
+            4 -> break
+            else -> println(i)
+        }
+    }
 }
