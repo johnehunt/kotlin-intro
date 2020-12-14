585cde2e5fb5262b088188456d7e51b46e685a58
John Hunt <john.hunt10@gmail.com>
2020-12-14 15:13:31 +0000

    Use of break and continue for Kotlin 1.4

---
+++ 03-objects-and-control/src/com/jjh/control/TestWhenWithBreakContinue.kt
@@ -0,0 +1,11 @@
+package com.jjh.control
+
+fun main() {
+//    for (i in 1..10) {
+//        when (i) {
+//            2 -> continue
+//            4 -> break
+//            else -> println(i)
+//        }
+//    }
+}
