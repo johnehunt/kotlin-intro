37fbfd46b6b611ae278f1a02bd861b3119b09f22
John Hunt <john@matraining.com>
2020-12-28 10:54:17 +0000

    Added a comment for the use of an anonymous object

--- 03-objects-and-control/src/com/jjh/anon/AnonymousObjectApp.kt
+++ 03-objects-and-control/src/com/jjh/anon/AnonymousObjectApp.kt
@@ -1,10 +1,14 @@
 package com.jjh.anon
 
 fun main() {
+
+    // Defines a new object which is anonymou
     val obj = object {
         var x: Int = 0
         val y = 42
     }
+
+    // Use single instance of anonymous object
     println(obj)
     println("obj.x = ${obj.x}, obj.y = ${obj.y}")
     obj.x = 50
