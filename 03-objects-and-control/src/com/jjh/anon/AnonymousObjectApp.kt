21fcd5db302f6f510ef4639eec5fc7139bb9a80c
John Hunt <john.hunt10@gmail.com>
2021-01-13 14:36:14 +0000

    Expanded example for an anonymous object

--- 03-objects-and-control/src/com/jjh/anon/AnonymousObjectApp.kt
+++ 03-objects-and-control/src/com/jjh/anon/AnonymousObjectApp.kt
@@ -6,11 +6,23 @@ fun main() {
     val obj = object {
         var x: Int = 0
         val y = 42
+
+        val MAX: Int
+
+        init {
+            MAX = 100
+        }
+
+        fun printMe() {
+            println("Print Me")
+        }
     }
 
     // Use single instance of anonymous object
     println(obj)
+    println(obj.MAX)
     println("obj.x = ${obj.x}, obj.y = ${obj.y}")
     obj.x = 50
     println("obj.x = ${obj.x}, obj.y = ${obj.y}")
+    obj.printMe()
 }
