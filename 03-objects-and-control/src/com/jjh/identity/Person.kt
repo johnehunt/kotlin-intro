5ea0d877b8380f7d67129041d3f2f6208d2b121f
John Hunt <john.hunt10@gmail.com>
2021-01-15 17:45:14 +0000

    Simplified ot focus on identity

--- 03-objects-and-control/src/com/jjh/identity/Person.kt
+++ 03-objects-and-control/src/com/jjh/identity/Person.kt
@@ -1,23 +1,12 @@
 package com.jjh.identity
 
-class Person(val name: String, val age: Int) {
-    override fun equals(other: Any?): Boolean {
-        return when (other) {
-            null -> false
-            is Person -> 
-                ((other.name == name) && (other.age == age))
-            else -> false
-        }
-    }
-}
+class Person(val name: String, val age: Int)
 
 fun main() {
     val p1 = Person("John", 21)
     val p2 = Person("John", 21)
     val p3 = p1
     println("p1 === p2: ${p1 === p2}") // checks for reference equality
-    println("p1 == p2: ${p1 == p2}") // checks for structural equality
-    println("p1.equals(p2): ${p1.equals(p2)}") // again structural equality
     println("p1 !== p2: ${p1 !== p2}")
     println("p1 === p3: ${p1 === p3}")
     println("p1 !== p3: ${p1 !== p3}")
