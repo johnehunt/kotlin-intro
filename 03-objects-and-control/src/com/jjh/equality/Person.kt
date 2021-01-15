47fbe4d9be6aca1f80d9cd893680f73d19dc4a7d
John Hunt <john.hunt10@gmail.com>
2021-01-15 17:43:53 +0000

    Re-written equals method as idiomatic kotlin

--- 03-objects-and-control/src/com/jjh/equality/Person.kt
+++ 03-objects-and-control/src/com/jjh/equality/Person.kt
@@ -2,10 +2,12 @@ package com.jjh.equality
 
 class Person(val name: String, val age: Int) {
     override fun equals(other: Any?): Boolean {
-        if (other == null) return false
-        if (other !is Person) return false
-        if (other.name != name && other.age != age) return false
-        return true
+        return when (other) {
+            null -> false
+            is com.jjh.identity.Person ->
+                ((other.name == name) && (other.age == age))
+            else -> false
+        }
     }
 
     override fun hashCode(): Int = name.hashCode() +  age
@@ -15,6 +17,7 @@ class Person(val name: String, val age: Int) {
 fun main() {
     val p1 = Person("John", 21)
     val p2 = Person("John", 21)
-    println("p1 == p2: ${p1 == p2}")
-    println("p1 != p2: ${p1 != p2}")
+    println("p1 === p2: ${p1 === p2}") // checks for reference equality
+    println("p1 == p2: ${p1 == p2}") // checks for structural equality
+    println("p1.equals(p2): ${p1.equals(p2)}") // again structural equality
 }
