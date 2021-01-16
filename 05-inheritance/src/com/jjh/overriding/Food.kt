2a30f212913de27305c17096724bebc12f114b6d
John Hunt <john.hunt10@gmail.com>
2021-01-16 12:19:56 +0000

    Illustrating how an open property can be closed off

--- 05-inheritance/src/com/jjh/overriding/Food.kt
+++ 05-inheritance/src/com/jjh/overriding/Food.kt
@@ -1,13 +1,19 @@
 package com.jjh.overriding
 
-open class Food {
+open class Food(open val type: String = "generic") {
     open fun eatMe() = println("Food - eatMe()")
 }
 
-open class Cake: Food() {
+open class Cake(final override val type: String = "Sponge"): Food() {
     final override fun eatMe() = println("Cake - eatMe()")
 }
 
-open class Biscuit: Food() {
+open class Biscuit(override val type: String = "Sweet"): Food() {
     override fun eatMe() = println("Biscuit - eatMe()")
+}
+
+fun main() {
+    val c = Cake()
+    println(c.type)
+    c.eatMe()
 }
